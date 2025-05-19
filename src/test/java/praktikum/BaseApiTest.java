82f402879184b8e0fe1e6619172536b0d9083aa9
Maksim Latyshev <maxlat2013@yandex.ru>
2025-05-19 18:02:48 +0500

    finish second review

--- src/test/java/praktikum/BaseApiTest.java
+++ src/test/java/praktikum/BaseApiTest.java
@@ -1,8 +1,6 @@
 package praktikum;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Before;
 import praktikum.user.UserModel;
