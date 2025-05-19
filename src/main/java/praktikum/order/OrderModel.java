15d97e1a9dc8b45a0cbd536925b626884902bdee
Maksim Latyshev <maxlat2013@yandex.ru>
2025-05-19 18:02:12 +0500

    add Lombok

--- src/main/java/praktikum/order/OrderModel.java
+++ src/main/java/praktikum/order/OrderModel.java
@@ -1,22 +1,13 @@
 package praktikum.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class OrderModel {
     private List<String> ingredients;
 
-    public OrderModel(List<String> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public OrderModel() {
-    }
-
-    public List<String> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<String> ingredients) {
-        this.ingredients = ingredients;
-    }
 }
